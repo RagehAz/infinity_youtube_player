2545278acc59a57373181ff4e56a72d79cd08820
spiercer <snowpiercer2001@gmail.com>
2025-08-05 08:44:21 +0300

    removed vertical floating list for simplicity and replaced with a column

--- lib/app/screens/a_home_screen.dart
+++ lib/app/screens/a_home_screen.dart
@@ -3,54 +3,36 @@ import 'package:infinity_youtube/core/layout/the_layout.dart';
 import 'package:infinity_youtube/core/shared_components/super_image/super_image.dart';
 import 'package:infinity_youtube/core/shared_components/super_text/super_text.dart';
 import 'package:infinity_youtube/core/theme/iconz.dart';
-import 'package:infinity_youtube/core/utilities/scale.dart';
-import 'package:infinity_youtube/core/utilities/vertical_floating_list.dart';
 
 class HomeScreen extends StatelessWidget {
   // --------------------------------------------------------------------------
-  const HomeScreen({
-    super.key
-  });
+  const HomeScreen({super.key});
   // -----------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
     // --------------------
-    final double _screenWidth = Scale.screenWidth(context);
-    final double _screenHeight = Scale.screenHeight(context);
-    // --------------------
-    return TheLayout(
-      child: VerticalFloatingList(
-        width: _screenWidth,
-        height: _screenHeight,
-        padding: const EdgeInsets.only(
-          top: Scale.appBarHeight + 10,
-          // bottom: 0,
-        ),
-        columnChildren: const <Widget>[
-
-          // --------------------
-
+    return const TheLayout(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
           SuperText(
             text: 'Click on the video link in the website to open the video',
             maxLines: 4,
             margins: 15,
             textHeight: 30,
           ),
-
           SuperImage(
-              width: 150,
-              height: 150,
-              pic: Iconz.logo,
-              corners: 20,
-              loading: false,
+            width: 150,
+            height: 150,
+            pic: Iconz.logo,
+            corners: 20,
+            loading: false,
           ),
-
-          // --------------------
-
         ],
       ),
     );
     // --------------------
   }
+
   // -----------------------------------------------------------------------------
 }
