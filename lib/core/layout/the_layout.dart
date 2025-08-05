85bcdea61d465399ac157516b541c838e8c37939
spiercer <snowpiercer2001@gmail.com>
2025-08-05 08:48:46 +0300

    refactor TheLayout to simplify structure and improve readability by removing unnecessary code

--- lib/core/layout/the_layout.dart
+++ lib/core/layout/the_layout.dart
@@ -1,51 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:infinity_youtube/core/layout/the_app_bar/the_app_bar.dart';
 import 'package:infinity_youtube/core/theme/colorz.dart';
-import 'package:infinity_youtube/core/utilities/scale.dart';
 
 class TheLayout extends StatelessWidget {
   // --------------------------------------------------------------------------
-  const TheLayout({
-    required this.child,
-    super.key
-  });
+  const TheLayout({required this.child, super.key});
   // --------------------
   final Widget child;
   // -----------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
-    // --------------------
-    final double _screenWidth = Scale.screenWidth(context);
-    final double _screenHeight = Scale.screenHeight(context);
-    // --------------------
     return Scaffold(
-      /// INSETS
-      resizeToAvoidBottomInset: false, /// if false : prevents keyboard from pushing pyramids up / bottom sheet
-      // resizeToAvoidBottomPadding: false,
       backgroundColor: Colorz.infinityLightGrey,
-      extendBodyBehindAppBar: true,
-      body: SizedBox(
-        width: _screenWidth,
-        height: _screenHeight,
-        child: Stack(
-          alignment: Alignment.topCenter,
-          children: <Widget>[
-
-            /// BODY
-            SizedBox(
-              width: _screenWidth,
-              height: _screenHeight,
-              child: child,
-            ),
-
-            /// APP BAR
-            const TheAppBar(),
-
-          ],
-        ),
-      ),
+      appBar: const TheAppBar(),
+      body: child,
     );
     // --------------------
   }
+
   // -----------------------------------------------------------------------------
 }
